1cbdcd143d7bb3754c28fe4fb26ed4cd9c2a8b6f
stas <msv262626@gmail.com>
2021-09-20 23:33:55 +0300

    add new imports in base_page.py add new method "is not element present" in base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,6 @@
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class BasePage:
@@ -29,3 +31,10 @@ class BasePage:
         except NoSuchElementException:
             return False
         return True
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
