7ebe78d7cc49c2523f9a5a76486e551614600085
StasMaslovski <msv262626@gmail.com>
2021-11-27 19:01:44 +0300

    change conftest.py

--- conftest.py
+++ conftest.py
@@ -3,26 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="chrome",
-                     help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default="en")
-
-
 @pytest.fixture(scope="function")
-def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': "ru, en"})
-        browser = webdriver.Chrome(options=options)
-
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+def browser():
+    options = Options()
+    browser = webdriver.Chrome()
     yield browser
     print("\nquit browser..")
     browser.quit()
